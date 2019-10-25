611cc1fa3b700b14870a49d4fd815c64b58bc32d
guilleavi <guillerminaavigliano@yahoo.com.ar>
2019-10-25 20:00:30 -0300

    create contacts module

---
+++ src/app/contacts/contacts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContactsRoutingModule } from './contacts-routing.module';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    ContactsRoutingModule
+  ]
+})
+export class ContactsModule { }
