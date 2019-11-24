f007c941a611db1aaa03eb85c0a2776dcba1b662
guilleavi <guillerminaavigliano@yahoo.com.ar>
2019-11-24 12:33:24 -0300

    update project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'contacts-manager'`, () => {
+  it(`should have as title 'contacts-manager-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('contacts-manager');
+    expect(app.title).toEqual('contacts-manager-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('contacts-manager app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('contacts-manager-client app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'contacts-manager';
+  title = 'contacts-manager-client';
 }
